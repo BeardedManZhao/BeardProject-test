bc3951bcec2d486ab132f9c046cef0f9ad7532a6
LingYuZhao <liming7887@qq.com>
2022-09-17 15:14:57 +0800

    zly
    这是一些测试的项目

---
+++ src/main/java/run_zhao/Hi.java
@@ -0,0 +1,12 @@
+package run_zhao;
+
+/**
+ * Java类于 2022/9/17 15:08:36 创建
+ *
+ * @author 4
+ */
+public class Hi {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
